6ac2382fc181713db7ea5aee2c783fdf5d601195
Johnny Wei <johntzwei@yahoo.com>
2019-11-16 11:14:17 -0800

    downloads

---
+++ data/download.sh
@@ -0,0 +1,4 @@
+wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt17-metrics-task-package.tgz -p downloads/
+wget http://computing.dcu.ie/~ygraham/newstest2017-segment-level-human.tar.gz -p downloads/
+wget http://computing.dcu.ie/~ygraham/newstest2017-system-level-human.tar.gz -p downloads/
+wget https://www.computing.dcu.ie/~ygraham/wmt16-metrics-results.tar.gz -p downloads/
