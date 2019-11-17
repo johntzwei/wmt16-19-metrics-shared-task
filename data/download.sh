4daf9ca0034727b62d10bd71cd547b5484d5b40b
Johnny Wei <johntzwei@yahoo.com>
2019-11-17 13:22:51 -0800

    all data loaders and validated

--- data/download.sh
+++ data/download.sh
@@ -2,3 +2,5 @@ wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt17-metrics-task-package.tgz -p down
 wget http://computing.dcu.ie/~ygraham/newstest2017-segment-level-human.tar.gz -p downloads/
 wget http://computing.dcu.ie/~ygraham/newstest2017-system-level-human.tar.gz -p downloads/
 wget https://www.computing.dcu.ie/~ygraham/wmt16-metrics-results.tar.gz -p downloads/
+wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt18-metrics-task-package.tgz -p downloads/
+wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt19-metrics-task-package.tgz -p downloads/
