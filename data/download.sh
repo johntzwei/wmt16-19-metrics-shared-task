ea696b333ac0948bc72eb7df82c11bb0312600a2
Johnny Wei <johntzwei@yahoo.com>
2019-12-01 15:22:20 -0800

    added all text for wmt16

--- data/download.sh
+++ data/download.sh
@@ -1,6 +1,7 @@
+wget https://www.computing.dcu.ie/~ygraham/wmt16-metrics-results.tar.gz -p downloads/
 wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt17-metrics-task-package.tgz -p downloads/
+
 wget http://computing.dcu.ie/~ygraham/newstest2017-segment-level-human.tar.gz -p downloads/
 wget http://computing.dcu.ie/~ygraham/newstest2017-system-level-human.tar.gz -p downloads/
-wget https://www.computing.dcu.ie/~ygraham/wmt16-metrics-results.tar.gz -p downloads/
 wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt18-metrics-task-package.tgz -p downloads/
 wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt19-metrics-task-package.tgz -p downloads/
