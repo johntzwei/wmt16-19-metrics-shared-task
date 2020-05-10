aba357a640e075e843e00771a10f0bab03d5f2e5
Johnny Wei <johntzwei@yahoo.com>
2020-05-09 20:09:31 -0700

    clean run of wmt17, added validation notebook

--- data/download.sh
+++ data/download.sh
@@ -1,8 +1,12 @@
-wget https://www.computing.dcu.ie/~ygraham/wmt16-metrics-results.tar.gz -p downloads/
-wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt17-metrics-task-package.tgz -p downloads/
-wget http://data.statmt.org/wmt17/translation-task/wmt17-submitted-data-v1.0.tgz -p downloads/
-wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt18-metrics-task-package.tgz -p downloads/
-wget http://data.statmt.org/wmt18/translation-task/wmt18-submitted-data-v1.0.1.tgz -p downloads/
-wget http://ufallab.ms.mff.cuni.cz/~bojar/wmt19-metrics-task-package.tgz -p downloads/
-wget http://data.statmt.org/wmt19/translation-task/wmt19-submitted-data-v3.tgz -p downloads/
-wget https://www.computing.dcu.ie/~ygraham/wmt19.newstest2019.HUMAN.de-en -p downloads/
+https://www.computing.dcu.ie/~ygraham/wmt16-metrics-results.tar.gz
+
+http://ufallab.ms.mff.cuni.cz/~bojar/wmt17-metrics-task-package.tgz
+http://data.statmt.org/wmt17/translation-task/wmt17-submitted-data-v1.0.tgz
+http://computing.dcu.ie/~ygraham/newstest2017-system-level-human.tar.gz
+
+http://ufallab.ms.mff.cuni.cz/~bojar/wmt18-metrics-task-package.tgz
+http://data.statmt.org/wmt18/translation-task/wmt18-submitted-data-v1.0.1.tgz
+
+http://ufallab.ms.mff.cuni.cz/~bojar/wmt19-metrics-task-package.tgz
+http://data.statmt.org/wmt19/translation-task/wmt19-submitted-data-v3.tgz
+https://www.computing.dcu.ie/~ygraham/wmt19.newstest2019.HUMAN.de-en
